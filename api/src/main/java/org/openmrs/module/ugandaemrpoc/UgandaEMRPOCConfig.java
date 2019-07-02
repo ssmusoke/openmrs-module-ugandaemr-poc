e2f9a18aed3e0d05fb6fae91e1c28d7061fa102b
Stephen S. Musoke <ssmusoke@gmail.com>
2019-07-02 21:22:22 +0300

    Initial commit of empty working Reference Application Module for UgandaEMR POC

---
+++ api/src/main/java/org/openmrs/module/ugandaemrpoc/UgandaEMRPOCConfig.java
@@ -0,0 +1,23 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.ugandaemrpoc;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Contains module's config.
+ */
+@Component("ugandaemrpoc.UgandaEMRPOCConfig")
+public class UgandaEMRPOCConfig {
+	
+	public final static String MODULE_PRIVILEGE = "UgandaEMR POC Privilege";
+	
+	public final static String MODULE_ID = "ugandaemrpoc";
+}
